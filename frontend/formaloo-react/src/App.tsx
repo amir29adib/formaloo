dababf258666d307fd51f83334d27c9926f8cac1
MightylilAl <alee.ansari@gmail.com>
2024-07-30 22:40:36 +0330

    modified:   frontend/formaloo-react/src/App.tsx 	modified:   frontend/formaloo-react/tsconfig.json

--- frontend/formaloo-react/src/App.tsx
+++ frontend/formaloo-react/src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { useForm } from 'react-hook-form';
+import { useForm, type FieldValues } from 'react-hook-form';
 
 export default function App() {
   const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data: FieldValues)  => console.log(data);
   console.log(errors);
   
   return (
