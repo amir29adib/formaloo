beaab68e3633fcc4eed1b49b908703b5c257177d
Arefe-Ehyaee <arefe.ehyaee@gmail.com>
2024-07-31 16:43:03 +0330

    deleted:    README.md 	modified:   frontend/formaloo-react/package.json 	modified:   frontend/formaloo-react/src/App.css 	modified:   frontend/formaloo-react/src/App.tsx 	new file:   frontend/formaloo-react/src/components/Dashboard.tsx 	new file:   frontend/formaloo-react/src/components/Home.tsx 	modified:   frontend/formaloo-react/yarn.lock 	new file:   frontend/yarn.lock

--- frontend/formaloo-react/src/App.tsx
+++ frontend/formaloo-react/src/App.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import { useForm, type FieldValues } from 'react-hook-form';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import './App.css';
 
 export default function App() {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -7,18 +11,36 @@ export default function App() {
   console.log(errors);
   
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="email" placeholder="email" {...register("email", {required: true})} />
-      <input type="text" placeholder="Username" {...register("Username", {required: true, max: 16, min: 6})} />
-      <input type="checkbox" placeholder="Checkbox" {...register("Checkbox", {required: true, min: 1})} />
-      <input type="checkbox" placeholder="checkbox" {...register} />
+    <div>
+      <BrowserRouter>
+        <nav>
+          <NavLink to='/'>Home</NavLink>
+          <NavLink to='/dashboard'>Dashboard</NavLink>
 
-      <input {...register("radio", { required: true })} type="radio" value="option 1" />
-      <input {...register("radio", { required: true })} type="radio" value=" option 2" />
-      <input {...register("radio", { required: true })} type="radio" value=" option 3" />
-      <input {...register("radio", { required: true })} type="radio" value=" option 4" />
+        </nav>
 
-      <input type="submit" />
-    </form>
+        <Routes>
+          <Route  path="/" element={<Home />} />
+          <Route  path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
+  
+      <form className='myForm' onSubmit={handleSubmit(onSubmit)}>
+        <input type="email" placeholder="email" {...register("email", {required: true})} />
+        <input type="text" placeholder="Username" {...register("Username", {required: true, max: 16, min: 6})} />
+        <div>
+          <input type="checkbox" placeholder="Checkbox" {...register("Checkbox", {required: true, min: 1})} />
+          <input type="checkbox" placeholder="checkbox" {...register} />
+          <input {...register("radio", { required: true })} type="radio" value="option 1" />
+          <input {...register("radio", { required: true })} type="radio" value=" option 2" />
+          <input {...register("radio", { required: true })} type="radio" value=" option 3" />
+          <input {...register("radio", { required: true })} type="radio" value=" option 4" />
+        </div>
+
+
+        <input type="submit" />
+      </form>
+
+    </div>
   );
 }
