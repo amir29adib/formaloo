168af6dd9eae8a1f759e6f86b2e01f296b5a9d8d
Arefe-Ehyaee <arefe.ehyaee@gmail.com>
2024-08-01 16:43:51 +0330

    modified:   frontend/formaloo-react/src/components/FormFields.tsx

--- frontend/formaloo-react/src/components/FormFields.tsx
+++ frontend/formaloo-react/src/components/FormFields.tsx
@@ -1,7 +1,27 @@
+import { useForm } from "react-hook-form";
 import { mockData } from "../MockData/mockData"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 
 export default function FormInput() {
+
+    const SignUpSchema = z.object({
+        username: z.string().min(3, "username must be at least 10 characters"),
+        email: z.string().email(),
+        
+      });
+      
+      type SignUpSchemaType = z.infer<typeof SignUpSchema>;
+    
+        
+      const {
+        register,
+        handleSubmit,
+        formState: { errors }
+      } = useForm<SignUpSchemaType>({
+        resolver: zodResolver(SignUpSchema),
+      });
 
 
     const inputFields = mockData.map((inputField) => {
@@ -11,6 +31,7 @@ export default function FormInput() {
                 type={inputField.type}
                 placeholder={inputField.placeholder}
                 value={inputField.value}
+                {...register("email")}
                 />
             </label>
         )
