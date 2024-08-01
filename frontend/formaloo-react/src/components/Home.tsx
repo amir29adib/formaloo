5c7ea8b3c6edc8bd3f5c2821182a5b6ba0d56b9e
Arefe-Ehyaee <arefe.ehyaee@gmail.com>
2024-08-01 14:21:03 +0330

    modified:   frontend/formaloo-react/src/App.css 	new file:   frontend/formaloo-react/src/MockData/mockData.tsx 	new file:   frontend/formaloo-react/src/components/FormFields.tsx 	modified:   frontend/formaloo-react/src/components/Home.tsx

--- frontend/formaloo-react/src/components/Home.tsx
+++ frontend/formaloo-react/src/components/Home.tsx
@@ -1,9 +1,12 @@
 import React from "react";
+import FormInput from "./FormFields";
 
 export default function Home() {
   return (
     <div>
       <h2>Home</h2>
+
+      <FormInput></FormInput>
     </div>
   );
 }
